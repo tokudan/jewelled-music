d84622fd7e77c4de40aabc4010f376e8cba9c6fb
Daniel Frank <git@danielfrank.net>
2014-03-01 19:05:05 +0100

    Add gem trollop to parse the options

--- Gemfile
+++ Gemfile
@@ -1,4 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'trollop'
+
 # Specify your gem's dependencies in jewelled-music.gemspec
 gemspec
--- bin/jewelled.rb
+++ bin/jewelled.rb
@@ -1,4 +1,16 @@
 #!/usr/bin/env ruby
-gem 'jewelled-music'
 require 'jewelled/music'
-p Dir.pwd
+require 'trollop'
+opts = Trollop::options do
+	opt :music,
+	    'Root directory of the music library',
+	    :type => :string
+	opt :target,
+	    'Sync music to target directory',
+	    :type => :string
+	opt :organize,
+	    'Organize the music directory'
+end
+Trollop::die :music, 'Music library must be specified' if opts[:music] == nil
+
+music_lib = Jewelled::Music::Library.new(opts[:music], {:organize => opts[:organize]})
