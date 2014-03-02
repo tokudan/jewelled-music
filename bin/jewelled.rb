caf8531c580cb9a19137f9a5d664b22cfc7887ed
Daniel Frank <git@danielfrank.net>
2014-03-02 21:54:04 +0100

    Prepare the command line interface for organizing the music library

--- bin/jewelled.rb
+++ bin/jewelled.rb
@@ -2,15 +2,31 @@
 require 'jewelled/music'
 require 'trollop'
 opts = Trollop::options do
+	banner <<-EOS
+Jewelled music organizes your music library and creates a mirror that contains lower bitrate files.
+	EOS
 	opt :music,
 	    'Root directory of the music library',
 	    :type => :string
+	opt :preview,
+	    'Only show a preview of what would have been done, do not move files or convert them.'
 	opt :target,
 	    'Sync music to target directory',
 	    :type => :string
 	opt :organize,
 	    'Organize the music directory',
 	    :type => :string
+	banner <<-EOS
+This option expects a string that will be used to build the target path for each file.
+It should contain variables that will be replaced by the information read from the tags of each file.
+Example: --organize "<album_artist>/<album>/<disc:02>-<track:02> <title>"
+Common variables that can be found in music files:
+album, album_artist, artist, date, disc, genre, title, track
+Special formatting options:
+<number>    Force that variable to a specific length. If the number starts with a zero, the variable will be considered
+			padded with zeros in front, if necessary. If there is a space in front of the number, it will be padded with
+			spaces instead.
+	EOS
 end
 Trollop::die :music, 'Music library must be specified' if opts[:music] == nil
 
