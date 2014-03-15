66fcf13555bb0f9fdde2f4ee0c202be02058333a
Daniel Frank <git@danielfrank.net>
2014-03-15 11:54:08 +0100

    Reorder the scanning of the metadata

--- lib/jewelled/music/track.rb
+++ lib/jewelled/music/track.rb
@@ -17,18 +17,20 @@ module Jewelled
 			end
 
 			def read_metadata
+				@info = Hash.new
 				# TODO: Maybe use a library for this.
 				out = String.new
 				Open3.popen3(%w(avconv avconv), '-i', @path) { |i, _, e, _|
 					i.close
 					out += e.read unless e.eof?
 				}
+				return unless out.include?('Metadata:')
+				return unless out.include?('Duration:')
 				# Now slice away everything but the metadata
 				metadata_start = out.index('Metadata:')
 				metadata_end = out.index('  Duration:')
 				metadata = out[(metadata_start+10)..(metadata_end)]
 				# And finally parse the metadata
-				info = Hash.new
 				metadata.each_line { |line|
 					begin
 						line.chomp!
@@ -38,12 +40,11 @@ module Jewelled
 						value = line[(field_end+2)..-1].strip
 						continue unless value
 						field.downcase!
-						info[field] = value
+						@info[field] = value
 					rescue
 						nil
 					end
 				}
-				@info = info
 			end
 		end
 	end
